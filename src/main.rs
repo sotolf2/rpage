9a31f164cc1bc5540da385f48e45f734dbe29caa
Sotolf2 <trym.karlsen@protonmail.ch>
2020-12-20 20:04:13 +0100

    Can now take file and show contents
    We can now show the contents of a file, and scroll up and down

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 mod event;
 
 use std::{error::Error, io};
-use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use std::fs::read_to_string;
+use std::env;
+use termion::{event::Key, raw::IntoRawMode};
 use event::{Event, Events};
 use tui::{
     backend::TermionBackend,
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::Span,
-    widgets::{Block, BorderType, Borders, Paragraph},
+    layout::{Constraint, Layout},
+    widgets::{Block, BorderType, Borders, Paragraph, Wrap},
+    style::Style,
     Terminal,
 };
 
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let file_name = &args[1];
+    let file_content = read_to_string(file_name)?;
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
     let events = Events::new();
+    let mut scroll: u16 = 0;
 
     terminal.clear()?;
     loop {
         terminal.draw(|f| {
-            // Wrapping block for a group
-            // Just draw the block and the group on the same area and build the group
-            // with at least a margin of 1
             let size = f.size();
             let block = Block::default()
                 .borders(Borders::ALL)
-                .title("Hello")
+                .title(String::from(file_name))
                 .border_type(BorderType::Rounded);
             f.render_widget(block, size);
 
             let layout = Layout::default()
-                            .margin(15)
+                            .margin(2)
                             .constraints(
                                 [
                                 Constraint::Percentage(100)
@@ -41,9 +44,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                             )
                             .split(size);
     
-            let paragraph = Paragraph::new("Hello, World!")
+            let paragraph = Paragraph::new(String::from(&file_content))
                             .style(Style::default())
-                            .alignment(tui::layout::Alignment::Center);
+                            .alignment(tui::layout::Alignment::Left)
+                            .wrap(Wrap { trim: true })
+                            .scroll((scroll, 0));
 
             f.render_widget(paragraph, layout[0]);
         })?;
@@ -52,7 +57,16 @@ fn main() -> Result<(), Box<dyn Error>> {
             if key == Key::Char('q') {
                 break;
             }
+            if key == Key::Down || key == Key::Char('j') {
+                scroll += 1;
+            }
+            if key == Key::Up || key == Key::Char('k') {
+                if scroll > 0 {
+                    scroll -= 1;
+                }
+            }
         }
     };
+    terminal.clear()?;
     Ok(())
 }
